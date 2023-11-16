f651398b794d6a64321ac18789cd291603a69aed
Shell Raiser <ksshailesh05@gmail.com>
2023-11-16 20:50:05 +0530

    init script setup

---
+++ shutdown.ps1
@@ -0,0 +1,13 @@
+# get admin access
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
+}
+
+# shutdown vm
+Stop-VM -Name kali-linux
+
+# Kill Xming, in this case vcxrv
+Get-process vcxsrv | Stop-Process
---
+++ startUp.ps1
@@ -0,0 +1,25 @@
+# Get Admin Access
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
+}
+
+# start VM
+Start-VM  kali-linux
+
+# Start Xming
+& 'C:\Program Files\VcXsrv\vcxsrv.exe' -multiwindow -dpi 150
+
+# wait for bootup
+do {
+	start-sleep -s 3
+
+	# extract the IP address and store it in a user environment variable
+	$output = get-vm -Name kali-linux | select -ExpandProperty networkadapters | select ipaddresses
+	$output = $output.IPAddresses[0]
+} until ($output -ne $null)
+
+# set the user env var
+[System.Environment]::SetEnvironmentVariable('KaliHyperVAddr',$output,[System.EnvironmentVariableTarget]::User)
