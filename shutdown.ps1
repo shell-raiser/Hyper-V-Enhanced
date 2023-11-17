c19f84b9db9a4aa13e99d5b84962402a6bf0fe90
Shell Raiser <ksshailesh05@gmail.com>
2023-11-17 08:24:45 +0530

    remove sleep as do-until is sufficient

--- shutdown.ps1
+++ shutdown.ps1
@@ -1,16 +1,16 @@
-# get admin access
+# Get Admin Access
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 $testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 if ($testadmin -eq $false) {
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) -WindowStyle Hidden
-exit $LASTEXITCODE
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) -WindowStyle Hidden
+    exit $LASTEXITCODE
 }
 
 # shutdown vm
-Stop-VM -Name kali-linux
+Stop-VM kali-linux
 
 # Kill Xming, in this case vcxrv
 Get-process vcxsrv | Stop-Process
 
 # remove the user env var
-[System.Environment]::SetEnvironmentVariable('KaliHyperVAddr',"",[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('KaliHyperVAddr', "", [System.EnvironmentVariableTarget]::User)
--- startUp.ps1
+++ startUp.ps1
@@ -2,24 +2,22 @@
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 $testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 if ($testadmin -eq $false) {
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) -WindowStyle Hidden
-exit $LASTEXITCODE
+	Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) -WindowStyle Hidden
+	exit $LASTEXITCODE
 }
 
 # start VM
-Start-VM  kali-linux
+Start-VM kali-linux
 
 # Start Xming
 & 'C:\Program Files\VcXsrv\vcxsrv.exe' -multiwindow -dpi 150
 
 # wait for bootup
 do {
-	start-sleep -s 3
-
 	# extract the IP address and store it in a user environment variable
 	$output = get-vm -Name kali-linux | Select-Object -ExpandProperty networkadapters | Select-Object ipaddresses
 	$output = $output.IPAddresses[0]
 } until ($null -ne $output)
 
 # set the user env var
-[System.Environment]::SetEnvironmentVariable('KaliHyperVAddr',$output,[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('KaliHyperVAddr', $output, [System.EnvironmentVariableTarget]::User)
