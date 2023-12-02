237a7ca5a3ed1c16eeb5ccbb9b09db4b732a0070
Shell Raiser <ksshailesh05@gmail.com>
2023-12-02 11:42:34 +0530

    terminal blocking inside the condition

--- hpe.ps1
+++ hpe.ps1
@@ -12,14 +12,13 @@ elseif ($args.Length -eq 0) {
     # check if vm already booted
     if ($null -eq [System.Environment]::GetEnvironmentVariable('KaliHyperVAddr', 'User')) {
         & $PSScriptRoot\startUp.ps1
-    }
-
-    # block terminal until operation is completed fully
-    do {
-        Start-Sleep -Seconds 1
-    } until (
-        $null -ne [System.Environment]::GetEnvironmentVariable('KaliHyperVAddr', 'User')
-    )
+        # block terminal until operation is completed fully
+        do {
+            Start-Sleep -Seconds 1
+        } until (
+            $null -ne [System.Environment]::GetEnvironmentVariable('KaliHyperVAddr', 'User')
+        )
+    }   
     # ssh into machine
     $ip = [System.Environment]::GetEnvironmentVariable('KaliHyperVAddr', 'User')
     ssh kss@$ip
